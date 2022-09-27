99f3097b349d9a66525e17368a829c2121cd09bb
Eugene Chirkun <evgenijchirkun@gmail.com>
2022-09-27 09:32:16 +0300

    First test extended

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -2,4 +2,8 @@
 
 it('Google Search', () => {
 cy.visit('https://google.com/')
+cy.get('.jw8mI').scrollTo('bottom')
+cy.get('#L2AGLb > .QS5gu').click()
+cy.get('.gLFyf').type("The Doors Riders on the storm video{enter}")
+cy.contains('Wideo').click()
 })

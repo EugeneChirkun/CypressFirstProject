dd6f41a2b8487743eed52f8f3f1d86a623c2b0c6
Eugene Chirkun <evgenijchirkun@gmail.com>
2022-09-27 12:28:34 +0300

    Explicit asserts

--- cypress/e2e/assertionsdemo.cy.js
+++ cypress/e2e/assertionsdemo.cy.js
@@ -7,4 +7,9 @@ it('Assertions Demo', () => {
         .and('have.class', 'query-btn')
         .and('be.visible')
         .and('be.enabled')
+
+    expect(true).to.be.true
+
+    assert.equal(4,'4', 'NOT EQUAL')
+    assert.strictEqual(4,'4', 'NOT EQUAL')
     })

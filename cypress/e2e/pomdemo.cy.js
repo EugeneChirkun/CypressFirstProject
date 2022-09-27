71528858d33588aaf8981fb594d93a834ed540b1
Eugene Chirkun <evgenijchirkun@gmail.com>
2022-09-27 14:32:08 +0300

    Test blocks

--- cypress/e2e/pomdemo.cy.js
+++ cypress/e2e/pomdemo.cy.js
@@ -1,14 +1,38 @@
 /// <reference types="cypress" />
-import {OrangeHrmLoginPage} from "./pages/orange_hrm_login_page"
+import { OrangeHrmLoginPage } from "./pages/orange_hrm_login_page"
 
 const loginPage = new OrangeHrmLoginPage()
 
-it('POM Demo', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/')
-    
-    loginPage.enterUsername('Admin')
-    loginPage.enterPassword('admin123')
-    loginPage.clickLogin()
-    
-    
+describe('All login tests', () => {
+
+    beforeEach(function() {
+        cy.visit('https://opensource-demo.orangehrmlive.com/')
     })
+
+    it('Login Test with all credentials correct', () => {
+        loginPage.enterUsername('Admin')
+        loginPage.enterPassword('admin123')
+        loginPage.clickLogin()
+
+        cy.contains('My Info').click()
+    })
+
+    it('Login Test with wrong UserName', () => {
+        loginPage.enterUsername('Admin13')
+        loginPage.enterPassword('admin123')
+        loginPage.clickLogin()
+
+        cy.contains('My Info').click()
+    })
+
+})
+
+it('Login Test with wrong Password', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/')
+
+    loginPage.enterUsername('Admin')
+    loginPage.enterPassword('admin1234')
+    loginPage.clickLogin()
+
+    cy.contains('My Info').click()
+})

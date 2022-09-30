51cd92ff36fc68d4c7b21f4d2213f2b6aedec002
Eugene Chirkun <evgenijchirkun@gmail.com>
2022-09-30 13:00:14 +0300

    writeFile()

--- cypress/e2e/readwritefiles.cy.js
+++ cypress/e2e/readwritefiles.cy.js
@@ -1,3 +1,4 @@
+/// <reference types="cypress" />
 before(function(){
     cy.fixture('example.json').as('test_data')
 })
@@ -15,4 +16,9 @@ it('Read file using readFile()', function() {
     cy.readFile('./cypress/fixtures/example.json').then((data) => {
         cy.log(data.name)
     })
+})
+
+it('Write file using writeFile()', function() {
+    cy.writeFile('sample.txt', 'My name is Eugene\n')
+    cy.writeFile('sample.txt', 'And I\'m learning Cypress', {flag: 'a+'})
 })
